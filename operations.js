9deda9bae07fdbf140406784203d6c442f8dd41f
Nisrine Sahouli <nisse@live.be>
2026-01-12 13:11:16 +0100

    Ajout comment hajar

--- operations.js
+++ operations.js
@@ -10,7 +10,7 @@ function subtract(a, b) {
 function multiply(a, b) {
     return a * b;
 }
-
+// Division Hajar t la
 function divide(a, b) {
     if (b === 0) {
         return "Erreur: division par zéro";
