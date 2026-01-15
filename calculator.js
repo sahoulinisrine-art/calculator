ae958ce7a253306c22e453f920a62869974b78b0
Nisrine Sahouli <nisse@live.be>
2026-01-15 10:19:23 +0100

    Ajout test calculator

--- calculator.js
+++ calculator.js
@@ -5,3 +5,9 @@ console.log("4 * 3 =", multiply(4, 3));
 console.log("10 / 2 =", divide(10, 2));
 
 console.log("Modification locale - test push");
+
+console.log("Hello World!"); 
+console.log("2 + 3 =", add(2, 3)); 
+console.log("5 - 2 =", subtract(5, 2)); 
+console.log("4 * 3 =", multiply(4, 3)); 
+console.log("3 * 7 =", multiply(3, 7));
